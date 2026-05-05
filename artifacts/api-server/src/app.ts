9763abd8dd1c1601d01d6b744d58f96d41596b42
sync-ctrl-doom <sydalam17@gmail.com>
2026-05-05 03:07:02 +0100

    fix: add .js extension to routes import

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -2,7 +2,7 @@ import express, { type Express } from "express";
 import cors from "cors";
 import pinoHttp from "pino-http";
 import type { IncomingMessage, ServerResponse } from "http";
-import router from "./routes/index";
+import router from "./routes/index.js";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
